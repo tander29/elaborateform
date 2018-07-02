69f8806292298fd231ce81bf8ffa31ba7e7a76e9
Travis Anderson <thetanderson@gmail.com>
2018-07-02 16:27:25 -0400

    completed basics

--- app.js
+++ app.js
@@ -10,5 +10,30 @@ app.use(express.static(publicFolderPath))
 const users = []
 
 // add POST request listener here
+app.post("/api/user", function (req, res) {
 
-app.listen(3000);
+    let userObject = req.body
+    console.log(userObject)
+    checkUserName(userObject, res)
+    console.log(users)
+
+})
+
+function checkUserName(userObject, res) {
+    console.log("checkusername")
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].userName === userObject.userName) {
+            // res.status(409)
+            res.send({ message: "Error, username Exists" })
+            return
+        }
+    }
+    userObject.idNumber = Math.floor(Math.random() * 18000)
+    users.push(userObject)
+    // res.status(201)
+    res.send(userObject)
+    // console.log(res)
+    return
+}
+
+app.listen(3000, function () { console.log("is this running??") });
--- public/index.js
+++ public/index.js
@@ -1,2 +1,36 @@
 const userCreateForm = document.getElementById("user-create-form")
 const userCreateSubmitButton = userCreateForm.querySelector("button[type='submit']")
+
+let userListObj = {}
+
+userCreateSubmitButton.addEventListener("click", action)
+
+function action(event) {
+    event.preventDefault()
+    const email = document.getElementById("email").value
+    const userName = document.getElementById("userName").value
+    const password = document.getElementById("password").value
+    const securityQuestion = document.getElementById("securityQuestion").value
+
+    userListObj.email = email
+    userListObj.userName = userName
+    userListObj.password = password
+    userListObj.securityQuestion = securityQuestion
+    console.log(userListObj)
+    console.log(JSON.stringify(userListObj))
+    const jsonUserListObj = JSON.stringify(userListObj)
+    postUser(jsonUserListObj)
+}
+
+function postUser(userInfo) {
+    console.log('again user', userInfo)
+    fetch(`./api/user`, {
+        method: 'POST',
+        body: userInfo,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(data => console.log('Reached Server, Server Message:', data))
+        .catch(error => console.error('Error:', error))
+}
